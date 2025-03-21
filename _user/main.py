e4878ae5f3649996f1f03986e4eeed155dca93f1
phamquiduong <phamquiduong@outlook.com>
2025-03-22 01:18:13 +0700

    Change openapi url

--- _user/main.py
+++ _user/main.py
@@ -2,5 +2,10 @@ from fastapi import FastAPI
 
 from routers.user import router as user_router
 
-app = FastAPI()
+app = FastAPI(
+    title="User API",
+    openapi_url='/users/openapi.json',
+    docs_url='/users/docs',
+    redoc_url='/users/redoc',
+)
 app.include_router(user_router)
